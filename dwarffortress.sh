4ec29dcbade28cccf8d6ef81b9fd15659c0df66b
Nitente <vfaria@protonmail.com>
2015-10-27 09:18:45 -0200

    add df

--- dwarffortress.sh
+++ dwarffortress.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-# BEFORE RUNNING THIS SCRIPT CHECK THE GAME VERSION IN bay12games.com/dwarves
-# AND CHANGE IT BELLOW ACCORDINGLY.
-
-# this script will download and 'install' dwarffortress for Fedora
-# it will change some files to search in data/art for .bmp instead of .png
-# the install location will be /home/$USER in a folder called 'Dwarf Fortress'
-# it will move any existing 'Dwarf Fortress' folder to 'Dwarf Fortress Backup'
-# it will create a script to launch dwarffortress in /bin/dwarffortress
-# it will create a shortcut 'Dwarves.desktop' inside 'Dwarf Fortress' folder
+# change the version bellow, check on bay12games.com/dwarves
+# this is for 64bit version Fedora
 
 version='40_24'
 dependencies=$(printf 'openal-soft.i686 glibc.i686 mesa-libGLU.i686 gtk2.i686 SDL.i686 SDL_image.i686 SDL_ttf.i686 libcanberra-gtk2.i686 gtk2-engines.i686')
@@ -24,20 +17,20 @@ cd dwarffortress_tmp
 echo "Downloading Dwarf Fortress."
 wget http://www.bay12games.com/dwarves/df_$(printf $version)_linux.tar.bz2
 tar -xvf df_$(printf $version)_linux.tar.bz2
-cp df_linux/df df_linux/dwarffortress
-chmod 754 df_linux/dwarffortress
+cp df_linux/df df_linux/df.original
+cp ../df df_linux/df
+cp ../df df_linux/dwarffortress
 sed -i '/cd "${DF_DIR}"/c\cd /home/$USER/Dwarf\\ Fortress/' df_linux/dwarffortress
-sed -i -e 's/png/bmp/g' df_linux/data/init/init.txt
-sed -i -e 's/mouse.png/mouse.bmp/g' df_linux/libs/Dwarf_Fortress
 mkdir /home/$USER/Dwarf\ Fortress/
 mv df_linux/* /home/$USER/Dwarf\ Fortress/
 echo -e "[Desktop Entry]\nName=Dwarves\nExec=/bin/dwarffortress\nType=Application\nTerminal=false" >> /home/$USER/Dwarf\ Fortress/Dwarves.desktop &&
 chmod 754 /home/$USER/Dwarf\ Fortress/Dwarves.desktop
+ln -s /home/$USER/Dwarf\ Fortress/Dwarves.desktop /home/$USER/.local/share/applications/
 cd ../
 rm -r dwarffortress_tmp 
 
 echo "It needs root permission for installing the dependencies and moving the launcher into /bin"
-sudo mv /home/$USER/Dwarf\ Fortress/dwarffortress /bin/
+sudo mv -f /home/$USER/Dwarf\ Fortress/dwarffortress /bin/
 sudo dnf install $dependencies
 
 echo "Everything should be peachy. Have FUN!"
